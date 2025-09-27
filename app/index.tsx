9c74abf14d3cec0783e2161f20dc36f624c72b0e
Tachera Sasi <tacherasasi@gmail.com>
2025-09-28 01:16:06 +0300

    refactor HomeScreen to use WebWrapper component; enhance WebWrapper with back navigation support

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-import { ThemedText } from "@/components/themed-text";
+import WebWrapper from "@/components/web-wrapper";
 import { View } from "react-native";
 
 export default function HomeScreen(){
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ThemedText>Home</ThemedText>
+      <View style={{ flex: 1 }}>
+        <WebWrapper/>
       </View>
     );
 }
--- components/web-wrapper.tsx
+++ components/web-wrapper.tsx
@@ -1,11 +1,45 @@
 import { WebView } from "react-native-webview";
 import Constants from "expo-constants";
-import { StyleSheet } from "react-native";
+import { BackHandler, Platform, StyleSheet } from "react-native";
 import { webUrl } from "@/constants/constants";
+import { useState, useRef, useCallback, useEffect } from "react";
 
 export default function WebWrapper() {
+
+    const [canGoBack, setCanGoBack] = useState(false);
+    const webViewRef = useRef(null);
+    const onAndroidBackPress = useCallback(() => {
+      if (canGoBack) {
+        webViewRef.current?.goBack();
+        return true; // prevent default behavior (exit app)
+      }
+      return false;
+    }, [canGoBack]);
+
+    useEffect(() => {
+      if (Platform.OS === "android") {
+        BackHandler.addEventListener("hardwareBackPress", onAndroidBackPress);
+        return () => {
+          BackHandler.removeEventListener(
+            "hardwareBackPress",
+            onAndroidBackPress
+          );
+        };
+      }
+    }, [onAndroidBackPress]);
   return (
-    <WebView style={styles.container} source={{ uri: webUrl }} />
+    <WebView
+      style={styles.container}
+      allowsBackForwardNavigationGestures={true}
+      allowFileAccess={true}
+      allowFileAccessFromFileURLs={true}
+      allowUniversalAccessFromFileURLs={true}
+      source={{ uri: webUrl }}
+      ref={webViewRef}
+      onLoadProgress={(event) => {
+        setCanGoBack(event.nativeEvent.canGoBack);
+      }}
+    />
   );
 }
 
